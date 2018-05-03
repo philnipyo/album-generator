03c7886fadefe0e0ff68daebf653b9a40a1de421
philnipyo <philnipyo@yahoo.com>
2018-05-02 19:51:43 -0700

    Change to Wikiepedia API instead of Mediawiki and properly access title value of the returned JSON object

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,11 @@ import axios from 'axios';
 
 async function getRandomPage() {
     try {
-        const result = await axios('https://cors-anywhere.herokuapp.com/https://www.mediawiki.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1');
+        const result = await axios('https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&format=json');
+        let title = result.data.query.random[0].title;
+
         console.log(result);
+        console.log(title);
     } catch(error) {
         console.log(error);
     }
