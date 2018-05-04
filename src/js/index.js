22e20a2996ab7147f76abe2e50191a974ff56065
philnipyo <philnipyo@yahoo.com>
2018-05-04 15:40:41 -0700

    Log random image url in console

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { CORS } from './config';
+import { unsplashAccessKey } from '../../api.js';
 
+// Get random page from Wikipedia API to serve as album title
 async function getRandomPage() {
     try {
         const result = await axios(`${CORS}https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&format=json`);
@@ -13,4 +15,18 @@ async function getRandomPage() {
     }
 }
 
+// Get random image from Unsplash API to serve as album image
+async function getRandomImage() {
+    try {
+        const result = await axios(`${CORS}https://api.unsplash.com/photos/random/?client_id=${unsplashAccessKey}`);
+        let image = result.data.urls.small;
+
+        console.log(result);
+        console.log(image);
+    } catch(error) {
+        console.log(error);
+    }
+}
+
 getRandomPage();
+getRandomImage();
