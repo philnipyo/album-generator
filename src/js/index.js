d8682a34333aa040c8ad1535c141ca0cc02b4428
philnipyo <philnipyo@yahoo.com>
2018-05-03 17:59:31 -0700

    Require CORS from new location

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { CORS } from './config';
 
 async function getRandomPage() {
     try {
-        const result = await axios('https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&format=json');
+        const result = await axios(`${CORS}https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&format=json`);
         let title = result.data.query.random[0].title;
 
         console.log(result);
