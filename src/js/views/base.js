9cd0633a60ce10794cdd464f15c2a49e3553f29a
philnipyo <philnipyo@yahoo.com>
2018-05-15 23:30:15 -0700

    Initial commit of DOM elements

---
+++ src/js/views/base.js
@@ -0,0 +1,6 @@
+export const elements = {
+    image: document.querySelector('.image'),
+    artist: document.querySelector('.artist'),
+    album: document.querySelector('.album'),
+    generate: document.querySelector('generate')
+};
