e78eb2f3d372faf2e3b27420ea368e51f1680494
philnipyo <philnipyo@yahoo.com>
2018-05-15 17:45:00 -0700

    Clean up index.js and separate async functions into separate modules

--- src/js/index.js
+++ src/js/index.js
@@ -1,49 +1,18 @@
 import axios from 'axios';
 import { CORS } from './config';
 import { unsplashAccessKey } from '../../api.js';
+import { getRandomPage } from './models/Artist';
+import { getRandomImage } from './models/Image';
+import { getRandomQuote } from './models/Album';
 
 // Get random page from Wikipedia API to serve as artist title
-async function getRandomPage() {
-    try {
-        const result = await axios(`${CORS}https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&format=json`);
-        let title = result.data.query.random[0].title;
-
-        console.log(title);
-    } catch(error) {
-        console.log(error);
-    }
-}
+getRandomPage();
 
 // Get random image from Unsplash API to serve as album image
-async function getRandomImage() {
-    try {
-        const result = await axios(`${CORS}https://api.unsplash.com/photos/random/?client_id=${unsplashAccessKey}`);
-        let image = result.data.urls.small;
-
-        console.log(image);
-    } catch(error) {
-        console.log(error);
-    }
-}
+getRandomImage();
 
 // Get random quote from Tadas Talaikis' API to serve as album name based on tbe last 3-5 words
-async function getRandomQuote() {
-    try {
-        const result = await axios('https://talaikis.com/api/quotes/random/');
-        let quote = result.data.quote;
-        let q = JSON.stringify(quote).split(' ');
-        let i = Math.floor((Math.random() * 3) + 3); //Better way to select the last 3-5 words; (max-min + 1) + min -> 5 - 3 + 1 = 3;
-
-        let name = q.slice(q.length - i, q.length).join(' ');
-        name = name.replace(".\"", '');
-        name = name.charAt(0).toUpperCase() + name.slice(1);
-
-        console.log(name);
-    } catch(error) {
-        console.log(error);
-    }
-}
-
-getRandomPage();
-getRandomImage();
 getRandomQuote();
+
+
+
