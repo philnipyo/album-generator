834fcabfa238d435a275a95c618b4665c572066d
philnipyo <philnipyo@yahoo.com>
2018-05-16 17:55:14 -0700

    Display Unsplash API image

--- src/js/models/Image.js
+++ src/js/models/Image.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
-import { unsplashAccessKey, CORS } from '../config'; 
+import { unsplashAccessKey, CORS } from '../config';
+import { elements } from '../views/base';
 
 export async function getRandomImage() {
     try {
         const result = await axios(`${CORS}https://api.unsplash.com/photos/random/?client_id=${unsplashAccessKey}`);
         let image = result.data.urls.small;
 
-        console.log(image);
+        elements.image.src=(image);
     } catch(error) {
         console.log(error);
     }
