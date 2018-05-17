b1b234b50cb46f34f640d180993cdaa9c61d7a68
philnipyo <philnipyo@yahoo.com>
2018-05-16 23:35:40 -0700

    Implement function to clear results

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,19 @@
 import axios from 'axios';
 import { CORS } from './config';
 import { unsplashAccessKey } from '../../api.js';
-import { getRandomPage } from './models/Artist';
-import { getRandomImage } from './models/Image';
-import { getRandomQuote } from './models/Album';
+import { getRandomPage, clearPage } from './models/Artist';
+import { getRandomImage, clearImage } from './models/Image';
+import { getRandomQuote, clearQuote } from './models/Album';
 import { elements } from './views/base';
 
 elements.generate.addEventListener('click', e => {
-    getRandomPage();
+    // Clear results if any
+    clearImage();
+    clearQuote();
+    clearPage();
+
+    // Fetch and render results
     getRandomImage();
+    getRandomPage();
     getRandomQuote();
 });
-
-// Get random page from Wikipedia API to serve as artist title
-
-
-// Get random image from Unsplash API to serve as album image
-
-
-// Get random quote from Tadas Talaikis' API to serve as album name based on tbe last 3-5 words
-
-
-
-
