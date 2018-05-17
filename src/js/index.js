d62604928aaa625b9b30f35559c7e52bc19df991
philnipyo <philnipyo@yahoo.com>
2018-05-16 17:55:53 -0700

    Rearrange API functions to be rendered when the generate button is clicked

--- src/js/index.js
+++ src/js/index.js
@@ -4,15 +4,22 @@ import { unsplashAccessKey } from '../../api.js';
 import { getRandomPage } from './models/Artist';
 import { getRandomImage } from './models/Image';
 import { getRandomQuote } from './models/Album';
+import { elements } from './views/base';
+
+elements.generate.addEventListener('click', e => {
+    getRandomPage();
+    getRandomImage();
+    getRandomQuote();
+});
 
 // Get random page from Wikipedia API to serve as artist title
-getRandomPage();
+
 
 // Get random image from Unsplash API to serve as album image
-getRandomImage();
+
 
 // Get random quote from Tadas Talaikis' API to serve as album name based on tbe last 3-5 words
-getRandomQuote();
+
 
 
 
