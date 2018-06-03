6ede3392dee09dd75529d1734fc9f5d027313500
philnipyo <philnipyo@yahoo.com>
2018-06-02 23:28:05 -0700

    Attribute photographer and Unsplash; create function to clear photographer info

--- src/js/models/Image.js
+++ src/js/models/Image.js
@@ -7,8 +7,14 @@ export async function getRandomImage() {
     try {
         const result = await axios(`${CORS}https://api.unsplash.com/photos/random/?client_id=${unsplashAccessKey}`);
         let image = result.data.urls.small;
+        let photographerName = result.data.user.name;
+        let photographerUserName = result.data.user.username
 
+        //Sets image src to JSON's small image result
         elements.image.src=(image);
+
+        // Inserts link to attribute photographer and Unsplash
+        elements.photographer.insertAdjacentHTML('afterbegin', `Photo by <a href="https://unsplash.com/@${photographerUserName}?utm_source=album_creator&utm_medium=referral">${photographerName}</a> on <a href="https://unsplash.com/?utm_source=album_creator&utm_medium=referral">Unsplash</a>`);
     } catch(error) {
         console.log(error);
     }
@@ -16,4 +22,8 @@ export async function getRandomImage() {
 
 export const clearImage = () => {
     elements.image.src='';
+}
+
+export const clearPhotographerInfo = () => {
+    elements.photographer.innerHTML= '';
 }
